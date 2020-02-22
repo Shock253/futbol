82f847fdec244259f48c7616954197fb8709a1a9
Steven Meyers <sjjmeyers@gmail.com>
2020-02-22 15:24:31 -0700

    Add test for average away goals

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -53,7 +53,6 @@ class TeamCollectionTest < Minitest::Test
   end
 
   def test_can_find_team_by_id
-    skip
     generic_team = mock('team')
     generic_team.stubs(:team_id).returns(2)
     specific_team = mock('specific')
@@ -124,11 +123,16 @@ class TeamCollectionTest < Minitest::Test
     assert_equal true, @team_collection.more_away_wins?(@game_collection, @los_angeles.team_id)
   end
 
+  def test_it_can_average_away_goals
+    assert_equal 4, @team_collection.average_away_goals(@game_collection, @los_angeles.team_id)
+  end
+
   def test_it_can_create_team_stats
     expected = {
       :total_games=>1,
       :games_won=>1,
       :home_games_won=>1,
+      :average_away_goals=>0,
       :away_games_won=>0,
       :winning_percentage=>100.0,
       :winning_difference_percentage=>0.0,
