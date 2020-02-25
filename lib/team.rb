ae130a37ff5253a5b7b87dcf5cda89dc9e91206e
Javier Aguilar <jvr.agx@gmail.com>
2020-02-24 17:40:37 -0700

    Clean up lines reported by linter and follow Ruby convention

--- lib/team.rb
+++ lib/team.rb
@@ -6,7 +6,7 @@ class Team
               :stadium,
               :link
 
-  def initialize (team_params)
+  def initialize(team_params)
     @team_id        = team_params[:team_id].to_i
     @franchiseId    = team_params[:franchiseid].to_i
     @teamName       = team_params[:teamname]
