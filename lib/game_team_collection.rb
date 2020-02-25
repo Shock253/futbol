9d57db98a308fd6372ee3f02d12e3c0a374fb865
Steven Meyers <sjjmeyers@gmail.com>
2020-02-24 18:22:22 -0700

    Refactor accuracy_ration to it's own method

--- lib/game_team_collection.rb
+++ lib/game_team_collection.rb
@@ -1,4 +1,5 @@
 require_relative './game_team'
+require_relative './game_collection'
 require 'CSV'
 
 class GameTeamCollection
@@ -23,8 +24,7 @@ class GameTeamCollection
 
   def games_by_season(game_collection, season)
     @game_teams.find_all do | game |
-      game_info = get_game_by_id(game_collection, game.game_id)
-      game_info.season == season
+      game.game_id.to_s[0..3] == season[0..3]
     end
   end
 
@@ -33,11 +33,26 @@ class GameTeamCollection
     games.sum { | game | game.tackles}
   end
 
+  def shots(season_info, team_id)
+    games = season_info.find_all { | game | game.team_id == team_id}
+    games.sum { | game | game.shots}
+  end
+
+  def goals_by_season(season_info, team_id)
+    games = season_info.find_all { | game | game.team_id == team_id}
+    games.sum { | game | game.goals}
+  end
+
+  def accuracy_ratio(season_info, team_id)
+    goals_by_season(season_info, team_id) / shots(season_info, team_id).to_f
+  end
+
   def season_stats(game_collection, season)
     season_info = games_by_season(game_collection, season)
     season_info.reduce({}) do | season_stats, game |
       season_stats[game.team_id] = {
-        tackles: tackles(season_info, game.team_id)
+        tackles: tackles(season_info, game.team_id),
+        accuracy_ratio: accuracy_ratio(season_info, game.team_id),
       }
       season_stats
     end
